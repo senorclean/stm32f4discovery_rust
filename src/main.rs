7765281b7243ee57dba68f136e874da77c0671ae
andrew <dontcare@gmail.com>
2021-01-08 11:25:48 -0800

    Scheduler setup with periodic function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![deny(unsafe_code)]
+// #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
@@ -8,61 +8,92 @@
 
 // use cortex_m;
 // use cortex_m_rt::entry;
-use stm32f4xx_hal as hal;
+use stm32f4xx_hal::{
+  prelude::*,
+  stm32,
+  gpio::gpiod,
+  gpio::Output,
+  gpio::PushPull,
+  delay::Delay,
+};
 
-use cortex_m_semihosting::{debug, hprintln};
+use rtic::cyccnt::{Instant, U32Ext};
+
+use cortex_m_semihosting::hprintln;
 use panic_semihosting as _;
-use crate::hal::{prelude::*, stm32};
 
-#[rtic::app(device = stm32f4xx_hal::stm32, peripherals = true)]
+#[rtic::app(
+  device = stm32f4xx_hal::stm32,
+  peripherals = true,
+  monotonic = rtic::cyccnt::CYCCNT
+)]
 const APP: () = {
-  #[init]
-  fn init(cx: init::Context) {
-    static mut X: u32 = 0;
+  struct Resources {
+    led: gpiod::PD12<Output<PushPull>>,
+  }
 
+  #[init(schedule = [blink])]
+  fn init(cx: init::Context) -> init::LateResources {
     // cortex-m peripherals
-    let _core: cortex_m::Peripherals = cx.core;
+//     let core: cortex_m::Peripherals = cx.core;
+// 
+//     core.DCB.enable_trace();
+//     core.DWT.enable_cycle_counter();
+// 
+//     let now = cx.start;
+// 
+//     let addr = 0xE000EDF0usize;
+//     let r = addr as *const u32;
+//     let debug_status = unsafe{*r & 0x00000001};
+// 
+//     if debug_status == 1 {
+//       hprintln!("init @ {:?}", now).unwrap();
+//     }
 
     // device specific peripherals
     // let _device: stm32::Peripherals = cx.device;
     let device: stm32::Peripherals = cx.device;
 
     let gpiod = device.GPIOD.split();
-    let mut led = gpiod.pd12.into_push_pull_output();
+    let led = gpiod.pd12.into_push_pull_output();
 
     let rcc = device.RCC.constrain();
-    let clocks = rcc.cfgr.sysclk(168.mhz()).freeze();
+    rcc.cfgr.sysclk(168.mhz()).freeze();
 
-    led.set_high().unwrap();
+    cx.schedule.blink(cx.start + 1_000.cycles()).unwrap();
 
-    // safe access to local 'static mut' variable
-    let _x: &'static mut u32 = X;
-
-    // hprintln!("init").unwrap();
-
-    debug::exit(debug::EXIT_SUCCESS);
+    init::LateResources {
+      led,
+    }
   }
 
-  // if let (Some(dp), Some(cp)) = (
-  //   stm32::Peripherals::take(),
-  //   cortex_m::peripheral::Peripherals::take(),
-  // ) {
-  //   // setup the led
-  //   let gpiod = dp.GPIOD.split();
-  //   let mut led = gpiod.pd12.into_push_pull_output();
+  #[task(schedule = [blink], resources = [led])]
+  fn blink(cx: blink::Context) {
+    // let now = Instant::now();
 
-  //   let rcc = dp.RCC.constrain();
-  //   let clocks = rcc.cfgr.sysclk(168.mhz()).freeze();
+    let high = cx.resources.led.is_set_high().unwrap();
+    if high {
+      cx.resources.led.set_low().unwrap();
+    } else {
+      cx.resources.led.set_high().unwrap();
+    }
 
-  //   let mut delay = hal::delay::Delay::new(cp.SYST, clocks);
+    cx.schedule.blink(cx.scheduled + 168_000_000.cycles()).unwrap();
+  }
 
-  //   loop {
-  //     led.set_high().unwrap();
-  //     delay.delay_ms(1000_u32);
-  //     led.set_low().unwrap();
-  //     delay.delay_ms(1000_u32);
-  //   }
-  // }
+//   #[idle(resources = [led, delay])]
+//   fn idle(cx: idle::Context) -> ! {
+// 
+//     loop {
+//       cx.resources.led.set_high().unwrap();
+//       cx.resources.delay.delay_ms(1000_u32);
+//       cx.resources.led.set_low().unwrap();
+//       cx.resources.delay.delay_ms(1000_u32);
+//     }
+//   }
 
-  // loop {}
+  extern "C" {
+    fn SDIO();
+  }
 };
+
